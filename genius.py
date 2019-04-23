465dfa89db9071d4fecbb2a26c4206ac252cc1ff
mitchellmarkoff <39682014+mitchellmarkoff@users.noreply.github.com>
2019-04-22 20:56:45 -0700

    Update genius.py

--- genius.py
+++ genius.py
@@ -4,7 +4,8 @@ import pprint
 genius = lyricsgenius.Genius("jUv62p0c58zg7c4fzIwTOYLLiw3whYP36jGhBOKnvmBjf2vkHeP49SU3X8vCMgc1")
 artist = genius.search_artist("Vanilla Ice", max_songs=3, sort="title")
 song = genius.search_song("Ice Ice Baby", artist.name)
-# search = genius.("a lot 21 savage lyrics")
-print(song.lyrics)
+#Uncomment Code to run search for specific song or keyword
+# search = genius.("you give love a bad name")
 # print(search)
 # pprint.pprint(search)
+print(song.lyrics)
